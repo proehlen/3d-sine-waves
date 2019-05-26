59673f7e9e8c317522e72deed8109cbbe63c519d
Peter Roehlen <peter.roehlen@gmail.com>
2019-05-26 14:30:09 +0700

    Plus/minus buttons on InputNumber

--- src/ui/InputNumberWithLabel.ts
+++ src/ui/InputNumberWithLabel.ts
@@ -1,4 +1,4 @@
-import { TextBlock, Container, InputText, Control } from '@babylonjs/gui';
+import { TextBlock, Container, InputText, Control, StackPanel, Button } from '@babylonjs/gui';
 
 export default class InputNumberWithLabel {
   _inputText: InputText;
@@ -13,18 +13,16 @@ export default class InputNumberWithLabel {
     focusedBackground: string = "lightblue",
   ) {
     const textBlock = new TextBlock(undefined, `${label}:`);
-    container.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
     textBlock.color = color;
     textBlock.paddingTopInPixels = 5;
     textBlock.heightInPixels = 30;
     container.addControl(textBlock);
+
     this._inputText = new InputText();
     this._inputText.color = color;
-    textBlock.paddingTopInPixels = 3;
-    textBlock.paddingBottomInPixels = 3;
     this._inputText.background = background;
-    this._inputText.heightInPixels = 31;
-    this._inputText.widthInPixels = widthInPixels;
+    this._inputText.heightInPixels = 34;
+    this._inputText.widthInPixels = widthInPixels - 30;
     this._inputText.focusedBackground = focusedBackground;
     this._inputText.onFocusSelectAll = true;
     this._inputText.onKeyboardEventProcessedObservable.add(
@@ -35,7 +33,44 @@ export default class InputNumberWithLabel {
       },
     );
     this._inputText.onBlurObservable.add((eventData: InputText) => onChange(Number(eventData.text)));
-    container.addControl(this._inputText);
+
+    // Increase button
+    const buttonIncrease = Button.CreateSimpleButton("buttonIncrease", "+");
+    buttonIncrease.widthInPixels = 18;
+    buttonIncrease.heightInPixels = 18;
+    buttonIncrease.background = "lightgrey";
+    buttonIncrease.fontSize = 12;
+    buttonIncrease.fontWeight = "bold";
+    buttonIncrease.onPointerUpObservable.add(() => {
+      onChange(Number(this._inputText.text) + 1);
+    });
+
+    // Decrease button
+    const buttonDecrease = Button.CreateSimpleButton("buttonDecrease", "-");
+    buttonDecrease.widthInPixels = 18;
+    buttonDecrease.heightInPixels = 18;
+    buttonDecrease.background = "lightgrey";
+    buttonDecrease.fontSize = 12;
+    buttonDecrease.fontWeight = "bold";
+    buttonDecrease.onPointerUpObservable.add(() => {
+      onChange(Number(this._inputText.text) - 1);
+    });
+
+
+    const verticalStack = new StackPanel();
+    verticalStack.isVertical = true;
+    verticalStack.widthInPixels = 20;
+    verticalStack.addControl(buttonIncrease);
+    verticalStack.addControl(buttonDecrease);
+
+    const horizontalStack = new StackPanel();
+    horizontalStack.isVertical = false;
+    horizontalStack.heightInPixels = 34;
+    horizontalStack.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
+    horizontalStack.addControl(this._inputText);
+    horizontalStack.addControl(verticalStack);
+
+    container.addControl(horizontalStack);
   }
 
   get value() {
