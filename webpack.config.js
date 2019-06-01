04387130d530a262b213da759c23faf99cd4c965
Peter Roehlen <peter.roehlen@gmail.com>
2019-06-01 21:57:29 +0700

    Add hash to webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = env => ({
     extensions: ['.ts', '.js'],
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'docs')
   }
 });
